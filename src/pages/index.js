be664be3112869e85318621e5c165e6601ff7a10
hreinberger <hannes.reinberger@payone.com>
2022-09-09 11:17:57 +0200

    fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/PAYONE API/Server API/">
+            to="/docs/payone-api/server-api/">
             Click here for 🚀
           </Link>
         </div>
